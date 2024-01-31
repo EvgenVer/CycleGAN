07f7a0f61a5dcbbcb90b7f95441a4205946dd1b9
EvgenVer <veremyev.e@gmail.com>
2024-01-31 16:45:14 +0530

    implement utils functions

--- src/train.py
+++ src/train.py
@@ -4,7 +4,8 @@ from tqdm.auto import tqdm
 
 def train_epoch(gen_trg, gen_src, dis_trg, dis_src, 
                 loader, opt_gen, opt_dis, l1, mse, 
-                dis_scal, gen_scal, lambda_cycle, device):
+                dis_scal, gen_scal, lambda_cycle, device,
+                sheduler_dis, sheduler_gen):
     loss_dis_ep = []
     loss_gen_ep = []
     real_score_ep = []
@@ -66,6 +67,9 @@ def train_epoch(gen_trg, gen_src, dis_trg, dis_src,
         gen_scal.update()
         loss_gen_ep.append(gen_loss.item())
         
+    sheduler_dis.step()
+    sheduler_gen.step()
+       
     gen_src.eval()
     gen_trg.eval()
     with torch.no_grad():
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import yaml
+import torch
 from torchvision.utils import make_grid
 import matplotlib.pyplot as plt
 
@@ -18,9 +19,38 @@ def show_images(img_tensors):
     ax.set_xticks([]); ax.set_yticks([])
     ax.imshow(img_tensors.permute(1, 2, 0))
     
-def save_checkpoint():
-    pass
-
-def load_checkpoint():
-    pass
+def save_checkpoint(gen_trg, gen_src, dis_trg, dis_src,
+                    opt_dis, opt_gen, chekpoints_path, epoch):
+    path = f'{chekpoints_path}/models_{epoch}_epoch'
+    torch.save({'generator_trg_state': gen_trg.state_dict(), 
+                'generator_src_state': gen_src.state_dict(),
+                'discriminator_trg_state': dis_trg.state_dict(),
+                'discriminator_src_state': dis_src.state_dict(),
+                'optimizer_dis_state': opt_dis.state_dict(),
+                'optimizer_gen_state': opt_gen.state_dict(),
+                'lr_opt_dis': opt_dis.param_groups[0]["lr"],
+                'lr_opt_gen': opt_gen.param_groups[0]["lr"],
+                'epoch': epoch}, path)
     
+
+def load_checkpoint(chekpoints_path, device, gen_trg=None, gen_src=None, 
+                    dis_trg=None, dis_src=None, opt_dis=None, 
+                    opt_gen=None, lr_opt_dis=False, lr_opt_gen=False):
+    chekpoint = torch.load(chekpoints_path, map_location=torch.device(device))
+    
+    if gen_trg:
+        gen_trg.load_state_dict(chekpoint['generator_trg_state'])
+    if gen_src:
+        gen_src.load_state_dict(chekpoint['generator_src_state'])
+    if dis_trg:
+        dis_trg.load_state_dict(chekpoint['discriminator_trg_state'])
+    if dis_src:
+        dis_src.load_state_dict(chekpoint['discriminator_src_state'])
+    if opt_dis:
+        opt_dis.load_state_dict(chekpoint['optimizer_dis_state'])
+    if opt_gen:
+        opt_gen.load_state_dict(chekpoint['optimizer_gen_state'])
+    if lr_opt_dis:
+        opt_dis.param_groups[0]["lr"] = chekpoint['lr_opt_dis']
+    if lr_opt_gen:
+        opt_gen.param_groups[0]["lr"] = chekpoint['lr_opt_gen']
